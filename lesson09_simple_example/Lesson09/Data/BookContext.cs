a5acec46426333663ca8075785c6b9ba48d20857
jear <jear@eaaa.dk>
2019-11-25 14:53:18 +0100

    Lesson 9 - simple example

---
+++ lesson09_simple_example/Lesson09/Data/BookContext.cs
@@ -0,0 +1,28 @@
+﻿using Lesson09.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lesson09.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Fluent API mappings (if needed)
+            modelBuilder.Entity<Book>().ToTable("Book");
+            modelBuilder.Entity<Author>().ToTable("Author");
+            
+            //Configure Book Column
+            modelBuilder.Entity<Book>()
+                        .Property(b => b.PublishedOn)
+                        .HasColumnType("datetime2");
+        }
+    }
+}
